f7069f1728e76772ce61cf52058cf413a1584a7b
Mustafa Zaki Assagaf <mustafa.segf@gmail.com>
2021-08-01 18:18:26 +0700

    fix: change base url

--- static/script.js
+++ static/script.js
@@ -1,4 +1,4 @@
-const base = window.location.origin;
+const base = window.location.href;
 
 console.log(base);
 
@@ -14,7 +14,7 @@ const addUrl = async (e) => {
     data.long_url = "http://" + data.long_url;
   }
   console.log("data", data);
-  const response = await fetch(`${base}/api/link/create`, {
+  const response = await fetch(`${base}api/link/create`, {
     method: "POST",
     body: JSON.stringify(data),
   });
@@ -22,7 +22,7 @@ const addUrl = async (e) => {
   console.log(resdata);
   const status = document.getElementById("status");
   if (response.status === 200) {
-    status.innerHTML = `url created! you can open using link <a target="_blank" href="${base}/${data.short_url}">${base}/${data.short_url} </a>`;
+    status.innerHTML = `url created! you can open using link <a target="_blank" href="${base}${data.short_url}">${base}${data.short_url} </a>`;
   } else if (response.status === 409) {
     status.innerHTML = "Short url already exist. Please use other url";
   } else if (response.status === 400) {
